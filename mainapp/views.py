6f4670be5fac9bba8b361cf1ac46efaafa0599e5
watsmanameha <62775429+watsmanameha@users.noreply.github.com>
2021-12-28 13:01:27 +0300

    lesson_1. Working static site (#1)

---
+++ mainapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def main(request):
+    return render(request, "mainapp/index.html")
+
+
+def products(request):
+    return render(request, "mainapp/products.html")
+
+
+def contact(request):
+    return render(request, "mainapp/contact.html")
+    
