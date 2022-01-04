fb4e887a9334ef9e3e9a009d21309d6f727da51f
watsmanameha <62775429+watsmanameha@users.noreply.github.com>
2022-01-04 23:07:23 +0300

    Lesson 2 (#2)
    * lesson_1. Working static site
    
    * Lesson 2. Templates
    
    * Lesson 2. Context for templates
    
    * Lesson 2. Repeat and filters

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,78 @@
+lesson_2
+import datetime
+
+
+master
 from django.shortcuts import render
 
 
 def main(request):
+lesson_2
+    title = "главная"
+    products = [
+        {
+            "name": "Отличный стул",
+            "desc": "Расположитесь комфортно.",
+            "image_src": "product-1.jpg",
+            "image_href": "/product/1/",
+            "alt": "продукт 1",
+        },
+        {
+            "name": "Стул повышенного качества",
+            "desc": "Не оторваться.",
+            "image_src": "product-2.jpg",
+            "image_href": "/product/2/",
+            "alt": "продукт 2",
+        },
+    ]
+    content = {"title": title, "products": products}
+    return render(request, "mainapp/index.html", content)
+
+
+def products(request):
+    title = "продукты"
+    links_menu = [
+        {"href": "products_all", "name": "все"},
+        {"href": "products_home", "name": "дом"},
+        {"href": "products_office", "name": "офис"},
+        {"href": "products_modern", "name": "модерн"},
+        {"href": "products_classic", "name": "классика"},
+    ]
+    same_products = [
+        {"name": "Отличный стул", "desc": "Не оторваться.", "image_src": "product-11.jpg", "alt": "продукт 11"},
+        {"name": "Стул повышенного качества", "desc": "Комфортно.", "image_src": "product-21.jpg", "alt": "продукт 21"},
+        {
+            "name": "Стул премиального качества",
+            "desc": "Просто попробуйте.",
+            "image_src": "product-31.jpg",
+            "alt": "продукт 31",
+        },
+    ]
+    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
+    return render(request, "mainapp/products.html", content)
+
+
+def contact(request):
+    title = "о нас"
+    visit_date = datetime.datetime.now()
+    locations = [
+        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
+        {
+            "city": "Екатеринбург",
+            "phone": "+7-777-777-7777",
+            "email": "info_yekaterinburg@geekshop.ru",
+            "address": "Близко к центру",
+        },
+        {
+            "city": "Владивосток",
+            "phone": "+7-999-999-9999",
+            "email": "info_vladivostok@geekshop.ru",
+            "address": "Близко к океану",
+        },
+    ]
+    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    return render(request, "mainapp/contact.html", content)
+
     return render(request, "mainapp/index.html")
 
 
@@ -11,4 +82,4 @@ def products(request):
 
 def contact(request):
     return render(request, "mainapp/contact.html")
-    
+    master
