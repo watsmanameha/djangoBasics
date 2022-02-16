60c5b8406e9904ceafc62e59e9280a13544cd094
watsmanameha <evgeshka120@gmail.com>
2022-02-16 23:01:36 +0300

    lesson_14. Code style fix

--- mainapp/migrations/0007_auto_20220216_1952.py
+++ mainapp/migrations/0007_auto_20220216_1952.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mainapp', '0006_product_is_active'),
+        ("mainapp", "0006_product_is_active"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='product',
-            name='is_active',
-            field=models.BooleanField(db_index=True, default=True, verbose_name='продукт активен'),
+            model_name="product",
+            name="is_active",
+            field=models.BooleanField(db_index=True, default=True, verbose_name="продукт активен"),
         ),
         migrations.AlterField(
-            model_name='productcategory',
-            name='is_active',
-            field=models.BooleanField(db_index=True, default=True, verbose_name='категория активна'),
+            model_name="productcategory",
+            name="is_active",
+            field=models.BooleanField(db_index=True, default=True, verbose_name="категория активна"),
         ),
     ]
--- ordersapp/migrations/0002_alter_order_is_active.py
+++ ordersapp/migrations/0002_alter_order_is_active.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ordersapp', '0001_orders'),
+        ("ordersapp", "0001_orders"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='order',
-            name='is_active',
-            field=models.BooleanField(db_index=True, default=True, verbose_name='активен'),
+            model_name="order",
+            name="is_active",
+            field=models.BooleanField(db_index=True, default=True, verbose_name="активен"),
         ),
     ]
